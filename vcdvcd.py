5edbadb5f4306ebe8715b7246d920070b01d0db5
Ciro Santilli <ciro.santilli@gmail.com>
2017-04-30 10:33:14 +0100

    Fix signal ordering, create store_tvs option

--- vcdvcd.py
+++ vcdvcd.py
@@ -15,6 +15,7 @@ class VCDVCD(object):
         print_deltas=False,
         print_dumps=False,
         signals=[],
+        store_tvs=True,
     ):
         """
         Parse a VCD file, and store information about it in this object.
@@ -22,6 +23,12 @@ class VCDVCD(object):
         The bulk of the parsed data can be obtained with :func:`parse_data`.
 
         :type vcd_path: str
+        :param store_tv: if False, don't store time values in the data
+                         Still parse them sequentially however, which may
+                         make them be printed if printing is enabled.
+                         This makes huge files more manageable, but prevents
+                         fast random access.
+        :type  store_tv: bool
         :param only_sigs: only parse the signal names under $scope and exit.
                         The return value will only contain the signals section.
                         This speeds up parsing if you only want the list of signals.
@@ -34,19 +41,20 @@ class VCDVCD(object):
         :param signals: only consider signals in this list.
                         If not given, all signals are considered.
                         Any printing done uses this signal order.
+                        If repeated signals are given, they are printed twice.
         :type  signals: List[str]
         :rtype: Dict[str,Any]
         """
         self._data = {}
         self._endtime = 0
-        self._signals = set()
-        self._value_changed = False
+        self._signals = []
+        self._store_tvs = store_tvs
 
-        signals = set(signals)
         all_sigs = not signals
         cur_sig_vals = {}
         hier = []
         num_sigs = 0
+        references_to_ids = {}
         time = 0
         with open(vcd_path, 'r') as f:
             while True:
@@ -73,21 +81,26 @@ class VCDVCD(object):
                 elif line0 == '#':
                     time = int(line[1:])
                     self._endtime = time
-                    if print_dumps: # and self._value_changed
+                    if print_dumps:
                         ss = []
                         ss.append('{}'.format(time))
-                        for identifier_code, value in cur_sig_vals.iteritems():
+                        for ref in print_dumps_refs:
+                            identifier_code = references_to_ids[ref]
+                            value = cur_sig_vals[identifier_code]
                             size = int(self._data[identifier_code]['size'])
                             ss.append('{0:>{1}s}'.format(value, size))
                         print(' '.join(ss))
-                    self._value_changed = False
                 elif '$enddefinitions' in line:
                     if only_sigs:
                         break
                     if print_dumps:
-                        print("0 time")
-                        for i, identifier_code in enumerate(cur_sig_vals.keys(), 1):
-                            print("{} {}".format(i, self._data[identifier_code]['references'][0] ))
+                        print('0 time')
+                        if signals:
+                            print_dumps_refs = signals
+                        else:
+                            print_dumps_refs = [self._data[i]['references'][0] for i in cur_sig_vals.keys()]
+                        for i,s in enumerate(print_dumps_refs, 1):
+                            print('{} {}'.format(i, s))
                         print()
                 elif '$scope' in line:
                     hier.append(line.split()[2])
@@ -98,11 +111,11 @@ class VCDVCD(object):
                     type = ls[1]
                     size = ls[2]
                     identifier_code = ls[3]
-                    name = "".join(ls[4:-1])
+                    name = ''.join(ls[4:-1])
                     path = '.'.join(hier)
                     reference = path + '.' + name
                     if (reference in signals) or all_sigs:
-                        self._signals.add(reference)
+                        self._signals.append(reference)
                         if identifier_code not in self._data:
                             self._data[identifier_code] = {
                                 'references': [],
@@ -110,6 +123,7 @@ class VCDVCD(object):
                                 'var_type': type,
                             }
                         self._data[identifier_code]['references'].append(reference)
+                        references_to_ids[reference] = identifier_code
                         if print_dumps:
                             cur_sig_vals[identifier_code] = 'x'
 
@@ -131,11 +145,12 @@ class VCDVCD(object):
 
     def get_signals(self):
         """
-        Get the set of unique signal names from the parsed VCD.
+        Get the set of unique signal names from the parsed VCD,
+        in the order they are defined in the file.
 
         This can be extracted from the data, but we also cache while parsing.
 
-        :rtype: Set[str]
+        :rtype: List[str]
         """
         return self._signals
 
@@ -145,11 +160,11 @@ class VCDVCD(object):
     ):
         if identifier_code in self._data:
             entry = self._data[identifier_code]
-            if 'tv' not in entry:
-                entry['tv'] = []
-            entry['tv'].append((time, value))
+            if self._store_tvs:
+                if 'tv' not in entry:
+                    entry['tv'] = []
+                entry['tv'].append((time, value))
             if print_deltas:
                 print("{} {} {}".format(time, value, entry['references'][0]))
             if print_dumps:
-                self._value_changed = True
                 cur_sig_vals[identifier_code] = value
