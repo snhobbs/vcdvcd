7c60be101c2cfea12598bce8a1c31415ca70b22f
Borja Penuelas <bmpenuelas@gmail.com>
2019-01-01 19:26:01 +0100

    Make package ready for PyPi, fix #1
    Great thanks to @bmpenuelas for helping with the change.

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
-from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.adoc') as f:
+        return f.read()
+
 setup(
     name='vcdvcd',
-    py_modules=['vcdvcd'],
+    version='1.0.3',
+    description='Python Verilog value change dump (VCD) parser library + the nifty vcdcat VCD command line viewer',
+    long_description=readme(),
+    url='https://github.com/cirosantilli/vcdvcd',
+    author='Ciro Santilli',
+    author_email='ciro.santilli.contact@gmail.com',
+    packages=find_packages(),
     scripts=['vcdcat'],
-    version='1.0',
 )
