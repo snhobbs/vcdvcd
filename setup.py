5dd4205c37ed0244ecaf443d8106fadb2f9cfbb8
Ciro Santilli <ciro.santilli@gmail.com>
2020-08-02 01:00:00 +0000

    v1.0.6
    Fix https://github.com/cirosantilli/vcdvcd/issues/5

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ def readme():
 
 setup(
     name='vcdvcd',
-    version='1.0.5',
+    version='1.0.6',
     description='Python Verilog value change dump (VCD) parser library + the nifty vcdcat VCD command line viewer',
     long_description=readme(),
+    long_description_content_type='text/plain',
     url='https://github.com/cirosantilli/vcdvcd',
     author='Ciro Santilli',
     author_email='ciro.santilli.contact@gmail.com',
